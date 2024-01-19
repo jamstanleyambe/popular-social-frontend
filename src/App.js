cd30dbc09f81f57cbaf42b909038dc1a2e36c136
jamstanleyambe <jamstanleyambe@gmail.com>
2024-01-20 00:07:28 +0100

    new commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import Header from './components/Header'
+function App() {
+  return (
+    <AppWrapper>
+<Header />
+    </AppWrapper >
+  );
+}
+const AppWrapper = styled.div`
+  background-color: #f1f2f5;
+`
+export default App;
