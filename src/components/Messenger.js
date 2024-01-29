624d7fae405f5d67b866c8be1d89c03d1e44ba84
jamstanleyambe <jamstanleyambe@gmail.com>
2024-01-29 01:45:55 +0100

    new

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -5,24 +5,62 @@ import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import styled from 'styled-components';
 import { useStateValue } from '../StateProvider'
-
+import axios from '../axios'
+import FormData from 'form-data'
 
 const Messenger = () => {
   const [input, setInput] = useState("");
   const [image, setImage] = useState(null);
   const [{ user }, dispatch] = useStateValue()
-  
+
   const handleChange = (e) => {
-    
     if (e.target.files[0]) setImage(e.target.files[0]);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (image) {
+      const imgForm = new FormData()
+      imgForm.append('file', image, image.name)
+      axios.post('/upload/image', imgForm, {
+        headers: {
+          'accept': 'application/json',
+          'Accept-Language': 'en-US,en;q=0.8',
+          'Content-Type': `multipart/form-data;
+                    boundary=${imgForm._boundary}`
+        }
+      }).then(res => {
+        const postData = {
+          text: input,
+          imgName: res.data.filename,
+          user: user.displayName,
+          avatar: user.photoURL,
+          timestamp: Date.now()
+        }
+        savePost(postData)
+      })
+    } else {
+      const postData = {
+        text: input,
+        user: user.displayName,
+        avatar: user.photoURL,
+        timestamp: Date.now()
+      }
+      savePost(postData)
+    }
+    setInput('')
+    setImage(null)
+  }
+  const savePost = async postData => {
+    await axios.post('/upload/post', postData)
+      .then(res => {
+        console.log(res)
+      })
+  }
   return (
     <MessengerWrapper>
       <MessengerTop>
-      <Avatar src={user.photoURL} />
+        <Avatar src={user.photoURL} />
         <form>
           <input
             type="text"
